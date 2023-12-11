a4d699fff57e8096c8399a061a18682b73d04713
oussamasenv <oussamaghazi.pf@gmail.com>
2023-12-11 15:06:48 +0100

    project creation

---
+++ src/test/java/com/tontine/tontine/TontineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tontine.tontine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
