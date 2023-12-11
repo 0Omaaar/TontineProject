1e7caf95628dc86356429adaf9dcb3167fd12828
oussamasenv <oussamaghazi.pf@gmail.com>
2023-12-11 17:06:48 +0100

    fen akhy

--- src/main/java/com/tontine/tontine/TontineApplication.java
+++ src/main/java/com/tontine/tontine/TontineApplication.java
@@ -8,7 +8,7 @@ public class TontineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TontineApplication.class, args);
-		System.out.println("hello oussama");
+		System.out.println("hello omar");
 	}
 
 }
