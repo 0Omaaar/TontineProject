ee3e10bc99d5a8a834fe8e5dff57f2bd264b754d
oussamasenv <oussamaghazi.pf@gmail.com>
2023-12-19 13:37:13 +0100

    19/12

---
+++ src/main/java/com/tontine/TontineApplicationCopy.java
@@ -0,0 +1,30 @@
+package com.tontine;
+
+import com.tontine.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineApplicationCopy
+//		implements CommandLineRunner
+{
+	@Autowired
+	private UserRepository UserRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineApplicationCopy.class, args);
+	}
+
+//	@Override
+//	public void run(String... args) throws Exception {
+////		User User = new User();
+////		User.setNomComplet("Yassir el haj");
+////		User.setEmail("Yassir@elhaj.com");
+////		User.setCin("AYB1888");
+////		User.setMotDePasse("YassirP@ssw0rdelhaj");
+////		User.setRole(User.Role.USER);
+////		User.setNumTele("0666666666");
+////		UserRepository.save(User);
+//	}
+}
