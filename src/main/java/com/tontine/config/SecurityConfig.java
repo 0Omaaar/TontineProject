580c1994050e9d85c75ceee77582557fc11f8c04
0Omaaar <elkhotriomarpro@gmail.com>
2023-12-23 23:00:08 +0100

    + 403 error page + admin dashboard template

--- src/main/java/com/tontine/config/SecurityConfig.java
+++ src/main/java/com/tontine/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.tontine.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,6 +17,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -26,6 +36,7 @@ public class SecurityConfig {
                 .authorizeRequests(auth -> auth
                         .requestMatchers("/","/css/**", "/login", "/register","/user/save", "/hello").permitAll()
                         .anyRequest().authenticated())
+                .exceptionHandling(eh -> eh.accessDeniedHandler(accessDeniedHandler()))
                 .httpBasic(withDefaults())
                 .formLogin(form -> form
                         .loginPage("/login"))
@@ -39,6 +50,22 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Component
+    public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+        @Override
+        public void handle(HttpServletRequest request, HttpServletResponse response, org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException, ServletException {
+            response.sendRedirect("/error-403");
+        }
+    }
 
     @Bean
     public UserDetailsService userDetailsService(){
---
+++ src/main/java/com/tontine/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.tontine.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/dashboard")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView dashboard(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin/dashboard");
+
+        return modelAndView;
+    }
+}
