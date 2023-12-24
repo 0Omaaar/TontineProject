d45f032da5ba5d2f72f41b3fe3d3f31b6c1d625b
0Omaaar <elkhotriomarpro@gmail.com>
2023-12-24 02:38:32 +0100

    + datatables to template

--- src/main/java/com/tontine/controller/AdminController.java
+++ src/main/java/com/tontine/controller/AdminController.java
@@ -1,12 +1,21 @@
 package com.tontine.controller;
 
+import org.springframework.ui.Model;
+import com.tontine.entities.Tontine;
+import com.tontine.service.TontineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
 public class AdminController {
+
+    @Autowired
+    private TontineService tontineService;
 
     @GetMapping("/dashboard")
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -16,4 +25,17 @@ public class AdminController {
 
         return modelAndView;
     }
+
+    @GetMapping("/tontines")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ModelAndView tontines(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+
+        List<Tontine> tontines = tontineService.findAll();
+        model.addAttribute("tontines", tontines);
+
+        modelAndView.setViewName("admin/tontines");
+        return modelAndView;
+    }
+
 }
